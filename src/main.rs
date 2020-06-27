d3bbc2b592d3957e9b15193186ed7f7fd618747e
Marcel Koch <kontakt@marcelkoch.net>
2020-06-28 00:23:32 +0200

    mock module in a module under test

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,19 @@ mod hello_world {
     }
 }
 
+mod under_test {
+    use super::hello_world::hello_world;
+
+    pub fn do_something() -> String {
+        hello_world()
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::hello_world::hello_world;
+    use super::under_test::do_something;
     use mocktopus::mocking::*;
 
     #[test]
@@ -34,6 +43,15 @@ mod tests {
 
         hello_world.mock_safe(|| MockResult::Return(String::from("Gude")));
         assert_eq!(hello_world(), "Gude");
+    }
+
+    #[test]
+    fn under_test() {
+
+        hello_world.mock_safe(|| MockResult::Return(String::from("Hello Mock")));
+
+
+        assert_eq!(do_something(), "Hello Mock");
     }
 }
 
